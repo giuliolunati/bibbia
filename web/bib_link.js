d22cb20c477e8e9d9604df18e873fec873fa2aa3
Giulio Lunati <giuliolunati@gmail.com>
2022-06-07 21:56:55 +0200

    bib_link.js: manage notes/ links and minor chamges

--- web/bib_link.js
+++ web/bib_link.js
@@ -1,16 +1,16 @@
 /* EXAMPLE:
  * [1] <a>Gv 1,1-5</a>
- * [2] <a>Giovanni cap. 1 (greco)|r=Gv 1&amp:v=g
+ * [2] <a>Giovanni cap. 1 (greco)#r=Gv 1&amp:v=g
  * <script src="bib_link.js"></script>
  *
- * OR WITH ID AS PREFIX:
+ * OR WITH SCRIPT ID AS PREFIX:
  * [1] <a>bib: Gv 1,1-5</a>
- * [2] <a>bib: Giovanni cap. 1 (greco)|r=Gv 1&amp:v=g
+ * [2] <a>bib: Giovanni cap. 1 (greco)#r=Gv 1&amp:v=g
  * <script id="bib:" src="bib_link.js"></script>
  *
  */
 
-var bib_link= function (url_bibbia_js, id){
+var bib_link= function (dir_bibbia_js, id){
   let l= document.getElementsByTagName('a')
   for (let o of l){
     let s,n
@@ -18,15 +18,20 @@ var bib_link= function (url_bibbia_js, id){
 		s= o.innerText
     if (id && !s.startsWith(id)) continue
     s= s.substring(id.length)
-    n= s.indexOf("|")
-    if (n >= 0) { // [2]
+    n= s.indexOf("#")
+    if (n == 0) {
+      s= s.substring(1)
+      o.innerHTML= s
+      s= s.toLowerCase().replace(/ /g, "_")
+      o.href= dir_bibbia_js +"/notes/" +s +".html"  
+    } else if (n > 0) { // [2]
       o.innerHTML= s.substring(0, n)
-      o.href = url_bibbia_js + "?" + s.substr(n+1)
+      o.href = dir_bibbia_js +"bibbia.html?" + s.substr(n+1)
     } else { // [1]
       o.innerHTML= s
         .replace(/_/g, " ")
       s= s.replace('?','&q=').replace(/\n/g, " ")
-      o.href= url_bibbia_js + "?v=i&r=" + s
+      o.href= dir_bibbia_js +"bibbia.html?v=i&r=" + s
     }
     o.target= "_blank"
     o.style.whiteSpace= "pre-line"
@@ -38,7 +43,7 @@ for (let x of document.getElementsByTagName("script")) {
   if (i >= 0) {
     let id
     if (x.id) id= x.id; else id= ""
-    bib_link(x.src.substring(0,i)+"bibbia.html", id)
+    bib_link(x.src.substring(0,i), id)
     break
   }
 }
